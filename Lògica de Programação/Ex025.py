ad8b0377c7a03309b04f6926864875157d2767d1
Gabriel Lagares <151885704+PhysicsManLG@users.noreply.github.com>
2025-09-24 16:45:11 -0300

    Add files via upload

---
+++ Lògica de Programação/Ex025.py
@@ -0,0 +1,19 @@
+'''
+Exiba os primeiros 10 termos da sequência de Fibonacci
+'''
+'''contador = 0
+soma = 1
+fibonacci = 0
+
+for contador in range(0, 11):
+    print(fibonacci)
+    proximo = fibonacci + soma
+    fibonacci = soma
+    soma = proximo
+
+while contador < 10:
+    print(fibonacci)
+    proximo = fibonacci + soma
+    fibonacci = soma
+    soma = proximo
+    contador += 1'''
---
+++ Lògica de Programação/Ex026.py
@@ -0,0 +1,29 @@
+'''
+Verifique se um número é primo usando um laço de repetição
+'''
+
+'''numero_primo = int(input("Digite um número qualquer maior que 1: "))
+contador = 2
+
+if numero_primo <= 1:
+    print(f"{numero_primo} não é primo.")
+else:
+    while contador < numero_primo:
+        if numero_primo % contador == 0:
+            print(f"{numero_primo} não é primo")
+            break
+        else:
+            contador += 1
+    else:
+        print(f"{numero_primo} é primo")
+
+if numero_primo <= 1:
+    print(f"{numero_primo} não é primo")
+else:
+    for contador in range(2, numero_primo):
+        if numero_primo % contador == 0:
+            print(f"{numero_primo} não é primo")
+            break
+
+    else:
+        print(f"{numero_primo} é primo")'''
