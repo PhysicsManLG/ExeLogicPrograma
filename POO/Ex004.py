c0607ecc1cbbbb08775530d26626d9c0e2c9277e
Gabriel Lagares <151885704+PhysicsManLG@users.noreply.github.com>
2025-09-24 16:58:26 -0300

    Ex004.py

--- POO/Ex004.py
+++ POO/Ex004.py
@@ -3,15 +3,36 @@ Crie um método falar() que exibe "Olá, meu nome é {nome}".
 '''
 
 class Pessoa:
-    def __init__(self, nome = ""):
+    def __init__(self, nome="", idade=0, email=""):
         self.nome = nome
+        self.idade = idade
+        self.email = email
     
-    def perguntar_nome(self):
-        self.nome = input("Olá, me informe o seu nome: ")
+    def pegar_informacoes(self):
+        self.nome = input("Informe seu nome: ")
+        self.idade = int(input("Informe sua idade: "))
+        self.email = input("Informe seu email: ")
+
+    def mostrar_informacoes(self):
+        print(f"Nome: {self.nome}, Idade: {self.idade} anos, Email: {self.email}")
+
+    def trocar_email(self):
+        while True:
+            novo_email = input("informe o novo email: ")
+            if novo_email == self.email:
+                print(f"email é o mesmo!")
+            else:
+                self.email = novo_email
+                print(f"O email atualizado com sucesso!\nSeu novo email é {self.email}")
+                break
     
     def falar(self):
-        print(f"Olá, me chamo {self.nome}")
+        print(f"Olá, meu nome é {self.nome}")
 
-pessoa = Pessoa()
-pessoa.perguntar_nome()
-pessoa.falar()
+
+pessoa1 = Pessoa()
+pessoa1.pegar_informacoes()
+pessoa1.mostrar_informacoes()
+pessoa1.trocar_email()
+pessoa1.mostrar_informacoes()
+pessoa1.falar()
